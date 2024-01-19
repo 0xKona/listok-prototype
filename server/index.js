d98e0fdceb07af73d1ab265858be4ee98c0d8865
Connor Robinson <konarobinson@outlook.com>
2024-01-19 18:30:03 +0000

    secrets folder added for PORT config and confidential information if needed in future

--- server/index.js
+++ server/index.js
@@ -1,18 +1,19 @@
 import express from 'express';
+import path from 'path';
 import dotenv from 'dotenv';
 import webpack from 'webpack';
 import webpackConfig from '../webpack-config.js';
 import webpackMiddleware from 'webpack-dev-middleware';
-import path from 'path';
 import * as url from 'url';
 import cors from 'cors';
 import bodyParser from 'body-parser';
+import apiRouter from './api-router.js';
 
 const __dirname = url.fileURLToPath(new URL('.', import.meta.url))
 
 const compiler = webpack(webpackConfig);
 
-dotenv.config();
+dotenv.config({ path: path.join(__dirname, './secrets/.env')});
 
 const app = express();
 const port = process.env.PORT;
@@ -23,6 +24,7 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '../client/dist'));
 app.use(express.static(path.join(__dirname, '../client/dist')));
 app.use(bodyParser.json())
+app.use('/api', apiRouter)
 app.get('/*', (req, res, next) => res.render('index'));
 
 app.listen(port, () => {
