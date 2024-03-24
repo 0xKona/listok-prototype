f5e25d8a9bf759381986488c85c9bb5143f458aa
0xKona <konarobinson@outlook.com>
2024-03-24 02:49:51 +0000

    step marked with green tick if complete

--- client/src/components/recipe-editor/step-tracker.tsx
+++ client/src/components/recipe-editor/step-tracker.tsx
@@ -1,13 +1,14 @@
 import { Step, StepLabel, Stepper } from "@mui/material";
 import React, { useState } from "react";
 import styled from "styled-components";
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
+import { StepIconProps } from '@mui/material/StepIcon';
 
 const Container = styled.div`
     width: 75%;
     display: flex;
     justify-content: center;
     align-items: center;
-    /* background-color: blue; */
 `
 
 const muiStepperStyle = {
@@ -20,14 +21,52 @@ interface props {
     setCurrentStep: React.Dispatch<React.SetStateAction<any>>;
 }
 
+interface Step {
+    label: string;
+    value: number;
+    complete: boolean;
+  }
+
 const StepTracker = ({steps, currentStep, setCurrentStep}: props) => {
+
+    const isStepComplete = (currentStep: number): boolean => {
+        const currentStepObj = steps.find(step => step.value === currentStep);
+        return currentStepObj ? currentStepObj.complete : false;
+      };
+      
+      const StepIcon: React.FC<StepIconProps> = ({ icon, active }) => {
+        // Ensuring 'icon' is always treated as a number
+        const stepNumber = typeof icon === 'number' ? icon : 0;
+        const completed = isStepComplete(stepNumber - 1); // Adjust for zero-based indexing if necessary
+    
+        if (completed) {
+          return <CheckCircleIcon style={{ color: 'green' }} />;
+        } else {
+          return (
+            <div
+              style={{
+                width: '24px',
+                height: '24px',
+                borderRadius: '50%',
+                backgroundColor: active ? '#3f51b5' : '#e0e0e0',
+                color: active ? '#fff' : '#000',
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+              }}
+            >
+              {stepNumber}
+            </div>
+          );
+        }
+      };
 
     return (
         <Container>
             <Stepper style={muiStepperStyle} activeStep={currentStep} alternativeLabel>
                 {steps.map((label: any) => (
                     <Step key={label.value} onClick={() => setCurrentStep(label)}>
-                        <StepLabel>{label.label}</StepLabel>
+                        <StepLabel StepIconComponent={StepIcon}>{label.label}</StepLabel>
                     </Step>
                 ))}
             </Stepper>
